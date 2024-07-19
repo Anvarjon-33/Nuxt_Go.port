e13ea90c13dc63bfc92cc936fe9a5282af11f02c
Anvarjon-33 <bagdoddonkadr1988@gmail.com>
2024-07-19 05:31:25 +0500

    Ending training for Nuxt3, (pages wildcat, slugs and so on)

---
+++ nuxt/pages/users/[[slug1]]/posts/you.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>
+    [[..]] -- > {{ useRoute().fullPath }}
+  </h1>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+</style>
---
+++ nuxt/pages/users/[slug2]/me.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>
+    [wild] > me page : path -> {{ route.fullPath }}
+  </h1>
+
+</template>
+
+<script lang="ts" setup>
+const route = useRoute()
+console.log(route.fullPath)
+</script>
+
+<style scoped>
+
+</style>
