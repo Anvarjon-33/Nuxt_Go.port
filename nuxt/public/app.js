dc7a3ca4ec60f458b0ae11a402aca13c88abb167
Anvarjon-33 <bagdoddonkadr1988@gmail.com>
2024-08-15 13:02:25 +0500

    cannot proxy nginx (from nuxt to golang api)

--- nuxt/public/app.js
+++ nuxt/public/app.js
@@ -23,7 +23,8 @@ function eraseCookie(name) {
     document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
 
-addEventListener("load", e => {
+addEventListener("load", async e => {
+
 })
 
 addEventListener("unload", e => {
